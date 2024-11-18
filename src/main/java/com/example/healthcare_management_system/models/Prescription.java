87ad5787c96617f08d32de1e07a67146882a46d9
JehanD11 <jehandesai121@gmail.com>
2024-11-18 18:13:33 +0530

    Prescription class completed

---
+++ src/main/java/com/example/healthcare_management_system/models/Prescription.java
@@ -0,0 +1,50 @@
+package com.example.healthcare_management_system.models;
+
+import java.util.*;
+import java.sql.*;
+import com.example.healthcare_management_system.models.*;
+import com.example.healthcare_management_system.DAO.*;
+import com.example.healthcare_management_system.databaseConnection;
+
+public class Prescription {
+    private int id;
+    private int patientId;
+    private int doctorId;
+    private String medication;
+    private String dosage;
+    private Timestamp issuedDate;
+
+    public Prescription(int patientId, int doctorId, String medication, String dosage, Timestamp issuedDate) {
+        this.patientId = patientId;
+        this.doctorId = doctorId;
+        this.medication = medication;
+        this.dosage = dosage;
+        this.issuedDate = issuedDate;
+    }
+
+    public Prescription(int id, int patientId, int doctorId, String medication, String dosage, Timestamp issuedDate) {
+        this.id = id;
+        this.patientId = patientId;
+        this.doctorId = doctorId;
+        this.medication = medication;
+        this.dosage = dosage;
+        this.issuedDate = issuedDate;
+    }
+
+    public int getId() {return this.id;}
+
+    public int getPatientId() {return this.patientId;}
+    public void setPatientId(int patientId) {this.patientId = patientId;}
+
+    public int getDoctorId() {return this.doctorId;}
+    public void setDoctorId(int doctorId) {this.doctorId = doctorId;}
+
+    public String getMedication() {return this.medication;}
+    public void setMedication(String medication) {this.medication = medication;}
+
+    public String getDosage() {return this.dosage;}
+    public void setDosage(String dosage) {this.dosage = dosage;}
+
+    public Timestamp getIssuedDate() {return this.issuedDate;}
+    public void setIssuedDate(Timestamp issuedDate) {this.issuedDate = issuedDate;}
+}
